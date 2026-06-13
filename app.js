627dc3c75f1805654fc7b23d52d4c46cbc40a634
bastianabimanyu <bastianabimanyu010105@gmail.com>
2026-06-13 16:58:25 +0700

    Update lagi boss

--- app.js
+++ app.js
@@ -1,9 +1,27 @@
-const nama = "Budi";
-let   umur = 28;
-let islogin = true;
+// Operator Aritmatika
+let a = 10;
+let b = 5;
 
-console.log(nama);
-console.log(umur);
-console.log(islogin);
+console.log(a + b); 
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(a % b);
 
 
+// Operator Perbandingan
+let nilai = 80;
+
+console.log(nilai > 70);
+console.log(nilai < 70);
+console.log(nilai === "80");
+console.log(nilai !== 90);
+
+
+// Operator Logika
+let usernamebenar = true;
+let passwordbenar = true;
+
+console.log(usernamebenar && passwordbenar);
+console.log(usernamebenar || passwordbenar);
+console.log(!usernamebenar);
